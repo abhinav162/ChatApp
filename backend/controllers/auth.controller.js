ee6a8e83642fbb221f7439c13670fdfc19126c5e
Abhinav Aditya <abhinavaditya162@gmail.com>
2024-06-06 17:56:47 +0530

    Updates

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -16,14 +16,14 @@ export const signup = async (req, res) => {
 			return res.status(400).json({ error: "Username already exists" });
 		}
 
-		// HASH PASSWORD HERE
+		// HASH PASSWORD
 		const salt = await bcrypt.genSalt(10);
 		const hashedPassword = await bcrypt.hash(password, salt);
 
-		// https://avatar-placeholder.iran.liara.run/
-
-		const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
-		const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+		// const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+		// const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+		const boyProfilePic = `https://api.dicebear.com/8.x/avataaars/svg?seed=Aneka`;
+		const girlProfilePic = `https://api.dicebear.com/8.x/avataaars/svg?seed=Felix`;
 
 		const newUser = new User({
 			fullName,
